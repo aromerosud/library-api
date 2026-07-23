4acef1229772509715764ca444d18a5c384457e0
Arturo Romero <romerosud89@gmail.com>
2026-07-23 17:18:48 -0500

    url default on production

--- swagger.js
+++ swagger.js
@@ -6,14 +6,15 @@ const options = {
     info: {
       title: "Library API",
       version: "1.0.0",
-      description: "API for managing books and authors. POST, PUT and DELETE endpoints require authentication through GitHub OAuth. Authenticate by visiting /login before testing protected endpoints.",
+      description:
+        "API for managing books and authors. POST, PUT and DELETE endpoints require authentication through GitHub OAuth. Authenticate by visiting /login before testing protected endpoints.",
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: "https://library-api-9nhf.onrender.com",
       },
       {
-        url: "https://library-api-9nhf.onrender.com/",
+        url: "http://localhost:3000",
       },
     ],
   },
